1166a4da78362b6566233b54678a308e949be0fd
Vadim Luzyanin <luzianin999@inbox.ru>
2020-07-06 14:56:13 +0300

    Updates

---
+++ precache-manifest.6bc5baa1f60f860003946d26e92f97e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ab4b860458223e30fe2a48b4bd0710",
+    "url": "/jsonplaceholder-gallery/index.html"
+  },
+  {
+    "revision": "fe39648175902cf86360",
+    "url": "/jsonplaceholder-gallery/static/css/main.69d689ca.chunk.css"
+  },
+  {
+    "revision": "db653849f8fe355022bd",
+    "url": "/jsonplaceholder-gallery/static/js/2.f3712ce0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jsonplaceholder-gallery/static/js/2.f3712ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe39648175902cf86360",
+    "url": "/jsonplaceholder-gallery/static/js/main.87a62fd8.chunk.js"
+  },
+  {
+    "revision": "beea6986dcb71d1218d8",
+    "url": "/jsonplaceholder-gallery/static/js/runtime-main.027362dc.js"
+  },
+  {
+    "revision": "07ad4b0e1b963b2033c14ff2aa0bcbfc",
+    "url": "/jsonplaceholder-gallery/static/media/loader.07ad4b0e.svg"
+  }
+]);
